83a53fc8aac6bd0eb099961c4a780661c16c0386
maso14af <maso14af@student.cbs.dk>
2015-12-02 21:27:16 +0100

    Oprettet MenuScreenController klasse - virker ikke endnu

--- src/Logic/MenuScreenController.java
+++ src/Logic/MenuScreenController.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
 
 
 
-public class LoginController
+public class MenuScreenController
 {
     public GUI.Screen screen;
 
@@ -17,7 +17,7 @@ public class LoginController
     /**
      * Opretter konstruktur. Laver objekt af screen.
      */
-    public LoginController()
+    public MenuScreenController()
     {
         screen = new Screen();
 
@@ -26,54 +26,13 @@ public class LoginController
         screen.setVisible(true);
     }//konstruktør slut
 
-    /**
-     * metoden Main klassen kaldes for at starte programmet
-     */
-    public void run()
-    {
-
-        // Injection af listeners i panels
-        screen.loginScreen.addActionListener(new LoginActionListener());
-        screen.getMenuScreen().addActionListener(new MenuScreenController.MenuScreenActionListener());
-
-        // vis startskaerm
-        screen.show(Screen.LOGINSCREEN);
-    }
-
-
-    private class LoginActionListener implements ActionListener
-    {
-        SDK.Logic logic = new SDK.Logic();
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {
-            //Når der trykkes på login
-            if (e.getSource() == screen.getLoginScreen().getBtnLogin())
-            {
-                String username = screen.getLoginScreen().getTxtUser().getText();
-                String password = screen.getLoginScreen().getTxtPassword().getText();
-
-                if(logic.login(username, password)) {
-
-                    screen.show(screen.MENUSCREEN);
-
-                }else {
-                    screen.getLoginScreen().getLblError().setVisible(true);
-                }
-
-
-
-
-            }//if listener slut
-        }//actionPerformed slut
-    }//inner class slut
 
     /**
      * inner class
      * UserMenu panel actionlistener
      * Styrer menu screen
      */
-    private class MenuScreenActionListener implements ActionListener
+    /*public class MenuScreenActionListener implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
@@ -81,20 +40,20 @@ public class LoginController
             //Hvis brugeren trykker på Create Game
             if (e.getSource() == screen.getMenuScreen().getBtnCreateGame())
             {
-            screen.show(Screen.CREATEGAMESCREEN);
+                screen.show(Screen.CREATEGAMESCREEN);
 
             }//if slut
 
             //Hvis brugeren trykker på delete game
             if (e.getSource() == screen.getMenuScreen().getBtnDeleteGame())
             {
-            screen.show(Screen.DELETEGAMESCREEN);
+                screen.show(Screen.DELETEGAMESCREEN);
             }//if slut
 
             //Hvis brugeren trykker på High Scores
             if (e.getSource() == screen.getMenuScreen().getBtnHighScore())
             {
-              screen.show(Screen.HIGHSCORESCREEN);
+                screen.show(Screen.HIGHSCORESCREEN);
 
 
             }//if slut
@@ -102,10 +61,10 @@ public class LoginController
             //Hvis brugeren trykker på Join Game .
             if (e.getSource() == screen.getMenuScreen().getBtnJoinGame())
             {
-            screen.show(Screen.JOINGAMESCREEN);
+                screen.show(Screen.JOINGAMESCREEN);
 
-               //Resetter evt. tekst i felterne.
-               // screen.getJoinGameScreen().getTxtGameName().setText("");
+                //Resetter evt. tekst i felterne.
+                // screen.getJoinGameScreen().getTxtGameName().setText("");
             }//if slut
 
             //hvis brugeren trykker log af knap
@@ -124,5 +83,6 @@ public class LoginController
     }//inner class slut
 
 
+}//Klasse slut
 
-}//klasse slut
+*/
