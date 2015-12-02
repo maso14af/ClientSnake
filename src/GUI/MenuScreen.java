d955eef34376c9d2e95fc59048416916effa6162
maso14af <maso14af@student.cbs.dk>
2015-12-02 20:56:09 +0100

    Trimning af kode i GUI.MenuScreen og sletning af home ikon fra MenuScreen

--- src/GUI/MenuScreen.java
+++ src/GUI/MenuScreen.java
@@ -2,12 +2,7 @@ package GUI;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Image;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,7 +16,6 @@ public class MenuScreen extends JPanel
 {
 	//Instantierer variabler
 	private JLabel lblMenu;
-	private JButton btnHome;
 	private JButton btnCreateGame;
 	private JButton btnHighScore;
 	private JButton btnDeleteGame;
@@ -30,9 +24,6 @@ public class MenuScreen extends JPanel
 
 
 
-	public JButton getBtnHome() {
-		return btnHome;
-	}
 
 	public JButton getBtnCreateGame() {
 		return btnCreateGame;
@@ -46,9 +37,7 @@ public class MenuScreen extends JPanel
 		return btnDeleteGame;
 	}
 
-	public JButton getBtnJoinGame() {
-		return btnJoinGame;
-	}
+	public JButton getBtnJoinGame() {return btnJoinGame;}
 
 	public JButton getBtnLogout() {
 		return btnLogout;
@@ -70,20 +59,6 @@ public class MenuScreen extends JPanel
 		setBounds(100, 100, 529, 475);
 		add(lblMenu);
 
-		//opretter,giver attributter og adder home knap
-		btnHome = new JButton();
-		try 
-		{
-			Image imgHome = ImageIO.read(getClass().getResource("/res/btnHome.png"));
-			btnHome.setIcon(new ImageIcon(imgHome));
-		} 
-		catch (IOException ex) 
-		{
-
-		}
-		btnHome.setBounds(16, 10, 42, 38);
-		add(btnHome);
-
 		//opretter,giver attributter og adder knap 
 		btnCreateGame = new JButton("Create Game");
 		btnCreateGame.setBounds(210, 103, 117, 29);
@@ -102,10 +77,6 @@ public class MenuScreen extends JPanel
 		add(btnLogout);
 		
 		btnJoinGame = new JButton("Join Game");
-		btnJoinGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		btnJoinGame.setBounds(210, 144, 117, 29);
 		add(btnJoinGame);
 
@@ -120,7 +91,6 @@ public class MenuScreen extends JPanel
 	 */
 	public void addActionListener(ActionListener l) 
 	{
-		btnHome.addActionListener(l);
 		btnCreateGame.addActionListener(l);
 		btnDeleteGame.addActionListener(l);
 		btnHighScore.addActionListener(l);
