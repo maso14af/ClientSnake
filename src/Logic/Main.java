f1c3a43cb5cbcb34ffea48f24f796496143c62c5
maso14af <maso14af@student.cbs.dk>
2015-12-05 15:52:07 +0100

    Ændring af main

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -1,7 +1,5 @@
 package Logic;
 
-import SDK.Logic;
-
 /**
  * Created by Martin on 09/11/2015.
  */
@@ -11,12 +9,8 @@ public class Main {
   public static void main(String[] args)
   {
 
-    Logic.Controller lg = new Logic.Controller();
-    Logic logic = new Logic();
-
-
-
-    lg.run();
+    Controller controller = new Controller();
+    controller.run();
 
   }
 
