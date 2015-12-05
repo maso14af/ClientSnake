3a9fa54e67d7d1656a46f783eaf28a1baca564e4
maso14af <maso14af@student.cbs.dk>
2015-12-05 12:43:37 +0100

    Logic work in progress

--- src/SDK/Logic.java
+++ src/SDK/Logic.java
@@ -11,31 +11,38 @@ import java.util.ArrayList;
 
 public class Logic {
 
-    User currentUser = new User ();
+    private User currentUser;
+    private ServerConnection serverConnection;
+
+    public Logic()
+    {
+        currentUser = new User();
+        serverConnection = new ServerConnection();
+    }
 
 
     public boolean login(String username, String password){
 
-        ServerConnection serverConnection = new ServerConnection();
+        //ServerConnection serverConnection = new ServerConnection();
 
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
+        currentUser.setUsername(username);
+        currentUser.setPassword(password);
 
 
-        String json = new Gson().toJson(user);
+        String json = new Gson().toJson(currentUser);
 
         if(serverConnection.post(json, "login/")==200){
 
-            for (User users : SDK.Logic.getUser()){
+            for (User users : getUser()){
 
-                if (users.getUsername().equals(username))
+                if (users.getUsername().equals(currentUser.getUsername()))
                 {
                     currentUser = users;
                 }
 
             }
 
+            System.out.print(currentUser.getUsername());
 
             return true;
         }else {
@@ -43,13 +50,13 @@ public class Logic {
         }
     }
 
-    public static ArrayList<User> getUser(){
+    public ArrayList<User> getUser(){
 
-        ServerConnection serverConnection = new ServerConnection();
+        //ServerConnection serverConnection = new ServerConnection();
 
         String json = serverConnection.get("users/");
 
-        //henter users ned i en arrayList
+        //Henter users ned i en arrayList
         ArrayList<User> users = new Gson().fromJson(json, new TypeToken<ArrayList<User>>(){}.getType());
 
         return users;
@@ -59,7 +66,7 @@ public class Logic {
 
     public boolean createGame(String name, String moves){
 
-        ServerConnection serverConnection = new ServerConnection();
+        //ServerConnection serverConnection = new ServerConnection();
 
         Gamer host = new Gamer();
         host.setId(currentUser.getId());
@@ -76,11 +83,33 @@ public class Logic {
 
         int response = serverConnection.post(json,"games/");
 
+        for(Game g : openGames())
+        {
+            if(g.getName().equals(name))
+            {
+                game.setGameId(g.getGameId());
+            }
+        }
+
         if(response==201){
+
+            System.out.print(game.getHost().getUsername());
             return true;
         }
 
         return false;
 
+    }
+
+    public ArrayList<Game> openGames()
+    {
+        //ServerConnection serverConnection = new ServerConnection();
+
+        String json = serverConnection.get("games/open/");
+
+        ArrayList<Game> openGames = new Gson().fromJson(json, new TypeToken<ArrayList<Game>>() {
+        }.getType());
+
+        return openGames;
     }
 }
