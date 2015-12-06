1063973a853c2f7eeac9202743469f03958ab417
maso14af <maso14af@student.cbs.dk>
2015-12-06 14:58:33 +0100

    Javadoc og comments for Logic klassen

--- src/SDK/Logic.java
+++ src/SDK/Logic.java
@@ -1,78 +1,114 @@
 package SDK;
 
-/**
- * Created by Martin on 18/11/15.
- */
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 
-public class Logic {
+/**
+ * This class controls the different requests and paths that are sent to the server.
+ * This class is used to connect to the Api class on the server.
+ */
 
-    User currentUser = new User();
+//Start of class
+public class Logic
+{
+
     ServerConnection serverConnection = new ServerConnection();
 
 
-
-
+    /**
+     * Method used to login to the server, the method posts an object of User in the gson format
+     * which is parsed to json.
+     * @param user posts an object of user
+     * @return json
+     */
     public int login(User user)
     {
         int json = serverConnection.post(new Gson().toJson(user), "login/");
-
         return json;
     }
 
-    public ArrayList<User> getUsers() {
+    /**
+     * Method using a get on the path users/ request to return all users and put them in an ArrayList.
+     * @return users
+     */
+    public ArrayList<User> getUsers()
+    {
         String jsonData = serverConnection.get("users/");
-        ArrayList<User> users = new Gson().fromJson(jsonData, new TypeToken<ArrayList<User>>() {
-        }.getType());
+        ArrayList<User> users = new Gson().fromJson(jsonData, new TypeToken<ArrayList<User>>() {}.getType());
         return users;
     }
 
+    /**
+     * Method using a post request to create a new game. The game object is send as Gson parsed to Json
+     * @param game object of game that is being posted.
+     * @return json
+     */
     public int createGame(Game game)
     {
         int json = serverConnection.post(new Gson().toJson(game), "games/");
         return json;
     }
 
+    /**
+     * Method using a put request to join an existing game. The game object is send as Gson parsed to Json
+     * @param game object of game that is being putted.
+     * @return json
+     */
     public int joinGame (Game game)
     {
         int json = serverConnection.put(new Gson().toJson(game), "games/join/");
         return json;
     }
 
+    /**
+     * Method using a put request to start a game. The game object is send as Gson parsed to Json.
+     * @param game object of game that is being putted.
+     * @return json
+     */
     public int startGame (Game game)
     {
         int json = serverConnection.put(new Gson().toJson(game), "games/start/");
         return json;
     }
 
+    /**
+     * Method using a get on the path games/open/ request to return all open games and put them in an ArrayList.
+     * @return openGames
+     */
     public ArrayList<Game> openGames()
     {
-        //ServerConnection serverConnection = new ServerConnection();
 
         String json = serverConnection.get("games/open/");
 
-        ArrayList<Game> openGames = new Gson().fromJson(json, new TypeToken<ArrayList<Game>>() {
-        }.getType());
+        ArrayList<Game> openGames = new Gson().fromJson(json, new TypeToken<ArrayList<Game>>(){}.getType());
 
         return openGames;
     }
 
+    /**
+     * Method using a delete request to delete a game from the server
+     * @param gameId
+     * @return json
+     */
     public int deleteGame (int gameId)
     {
         int json = serverConnection.delete("games/" +gameId+ "/");
         return json;
     }
 
+    /**
+     * Getter for highScores, is done making a get request to the server and return all highScores and
+     * putting them in an ArrayList so they can be used in the JTable to show it to the user.
+     * @return highScores
+     */
     public ArrayList<Score> getHighScores()
     {
         String json = serverConnection.get("highScores/");
-        ArrayList<Score> highScores = new Gson().fromJson(json, new TypeToken<ArrayList<Score>>() {
-        }.getType());
+        ArrayList<Score> highScores = new Gson().fromJson(json, new TypeToken<ArrayList<Score>>() {}.getType());
 
         return highScores;
     }
-}
+}//End of class Logic
