58dad817bca253ffac32def270b33181c55f3eb5
maso14af <maso14af@student.cbs.dk>
2015-12-06 15:06:34 +0100

    Javadoc og comments for Main klassen

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -2,10 +2,13 @@ package Logic;
 
 /**
  * Created by Martin on 09/11/2015.
+ * Main class used to start the whole program. This is done by running the run() method in the Controller class.
  */
 
 
-public class Main {
+public class Main
+{
+
   public static void main(String[] args)
   {
 
@@ -13,9 +16,4 @@ public class Main {
     controller.run();
 
   }
-
-
-
-
-
-}
+}//End of class Main
