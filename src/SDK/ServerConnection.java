3672bb2eab29c4c2bd8fe1da6855d3264fdb1338
maso14af <maso14af@student.cbs.dk>
2015-12-06 14:11:18 +0100

    Javadoc og comments for ServerConnection klassen

--- src/SDK/ServerConnection.java
+++ src/SDK/ServerConnection.java
@@ -6,34 +6,53 @@ import com.sun.jersey.api.client.WebResource;
 
 /**
  * Created by Martin on 18/11/15.
+ * This class handles the connection to the server. In this class there are getters for
+ * various server attributes as well as post, get, put and delete requests.
+ * The requests are made with Json.
  */
-public class ServerConnection {
+//Start of class
+public class ServerConnection
+{
+    //Declaring variables used in this class
+    private String hostAddress;
+    private int port;
 
-    public ServerConnection(){
+    /**
+     * Constructor for the serverConnection Class
+     */
+    public ServerConnection()
+    {
+        //Initializing of the variables used in the constructor
         this.hostAddress = "http://localhost";
         this.port = 8882;
     }
 
-    private String hostAddress;
-    private int port;
-
-    public void setHostAddress(String hostAddress) {
-        this.hostAddress = hostAddress;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getHostAddress() {
+    /**
+     * Getter for hostAddress
+     * @return hostAddress
+     */
+    public String getHostAddress()
+    {
         return hostAddress;
     }
 
-    public int getPort() {
+    /**
+     * Getter for port
+     * @return port
+     */
+    public int getPort()
+    {
         return port;
     }
 
-    public String get(String path){
+
+    /**
+     * Method for getting something from the server
+     * @param path is the path of what we want off of the server
+     * @return message
+     */
+    public String get(String path)
+    {
 
         Client client = Client.create();
 
@@ -45,7 +64,14 @@ public class ServerConnection {
         return message;
     }
 
-    public int post(String json, String path){
+    /**
+     * Method for posting something to the server
+     * @param json is the object we send to the server as json format
+     * @param path is the path of where we want to post something to the server
+     * @return responser
+     */
+    public int post(String json, String path)
+    {
 
         Client client = Client.create();
 
@@ -59,7 +85,14 @@ public class ServerConnection {
         return responser;
     }
 
-    public int put(String json, String path){
+    /**
+     * Method when putting something to the server
+     * @param json is the object we send to the server as json format
+     * @param path is the path of where we want to put something to the server
+     * @return responser
+     */
+    public int put(String json, String path)
+    {
 
         Client client = Client.create();
 
@@ -73,7 +106,13 @@ public class ServerConnection {
         return responser;
     }
 
-    public int delete(String path){
+    /**
+     * Method when deleting something from the server
+     * @param path the path of where we want to delete something from the server
+     * @return responser
+     */
+    public int delete(String path)
+    {
 
         Client client = Client.create();
 
@@ -86,4 +125,4 @@ public class ServerConnection {
 
         return responser;
     }
-}
+}//End of class
